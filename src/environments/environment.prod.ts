c159dc4e3f54b604f91be42aef1e948d22acea95
unknown <olga.garanko@gmail.com>
2021-01-04 19:15:05 +0200

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_KEY: 'AIzaSyAHMwHMFVyqUlbgSauhtREJA_DP3PBi5mc',
+  fbDbUrl: 'https://jiraclone-4a1e0-default-rtdb.europe-west1.firebasedatabase.app/'
+};
